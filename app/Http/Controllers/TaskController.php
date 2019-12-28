3ab79b7406c41553e7a15c63c1b1f748d343379c
MasanaIto <z3heng0326@gmail.com>
2019-12-28 16:28:26 +0900

    エラーハンドリングを実装

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,57 +11,73 @@ use Illuminate\Support\Facades\Auth;
 
 class TaskController extends Controller
 {
-    public function index(int $id)
+    /**
+     * タスク一覧
+     * @param Folder $folder
+     * @return \Illuminate\View\View
+     */
+    public function index(Folder $folder)
     {
-        // 全てのフォルダを取得する
+        // ユーザーのフォルダを取得する
         $folders = Auth::user()->folders()->get();
 
-        // 選ばれたフォルダを取得する
-        $current_folder = Folder::find($id);
-
         // 選ばれたフォルダに紐づくタスクを取得する
-        // $tasks = Task::where('folder_id', $current_folder->id)->get();
-        $tasks = $current_folder->tasks()->get();
+        $tasks = $folder->tasks()->get();
 
         return view('tasks/index', [
             'folders'           => $folders,
-            'current_folder_id' => $current_folder->id,
+            'current_folder_id' => $folder->id,
             'tasks'             => $tasks,
         ]);
     }
 
 
 
-    public function showCreateForm(int $id)
+    /**
+     * タスク作成フォーム
+     * @param Folder $folder
+     * @return \Illuminate\View\View
+     */
+    public function showCreateForm(Folder $folder)
     {
         return view('tasks/create', [
-            'folder_id' => $id,
+            'folder_id' => $folder->id,
         ]);
     }
 
 
 
-    public function create(int $id, CreateTask $request)
+    /**
+     * タスク作成
+     * @param Folder $folder
+     * @param CreateTask $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function create(Folder $folder, CreateTask $request)
     {
-        $current_folder = Folder::find($id);
-
         $task = new Task();
         $task->title = $request->title;
         $task->due_date = $request->due_date;
 
         // リレーションを生かしたデータの保存
-        $current_folder->tasks()->save($task);
+        $folder->tasks()->save($task);
 
         return redirect()->route('tasks.index', [
-            'id' => $current_folder->id,
+            'id' => $folder->id,
         ]);
     }
 
 
 
-    public function showEditForm(int $id, int $task_id)
+    /**
+     * タスク編集フォーム
+     * @param Folder $folder
+     * @param Task $task
+     * @return \Illuminate\View\View
+     */
+    public function showEditForm(Folder $folder, int $task_id)
     {
-        $task = Task::find($task_id);
+        // $this->checkRelation($folder, $task);
 
         return view('tasks/edit', [
             'task' => $task,
@@ -69,11 +85,17 @@ class TaskController extends Controller
     }
 
 
-    
-    public function edit(int $id, int $task_id, EditTask $request)
+
+    /**
+     * タスク編集
+     * @param Folder $folder
+     * @param Task $task
+     * @param EditTask $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function edit(Folder $folder, Task $task, EditTask $request)
     {
-        // リクエストされた ID でタスクデータを取得
-        $task = Task::find($task_id);
+        $this->checkRelation($folder, $task);
 
         // 編集対象のタスクデータに入力値を詰めて save 
         $task->title = $request->title;
@@ -85,5 +107,19 @@ class TaskController extends Controller
         return redirect()->route('tasks.index', [
             'id' => $task->folder_id,
         ]);
+    }
+
+
+
+    /**
+     * フォルダとタスクの関連性があるか調べる
+     * @param Folder $folder
+     * @param Task $task
+     */
+    private function checkRelation(Folder $folder, Task $task)
+    {
+        if ($folder->id !== $task->folder_id) {
+            abort(404);
+        }
     }
 }
